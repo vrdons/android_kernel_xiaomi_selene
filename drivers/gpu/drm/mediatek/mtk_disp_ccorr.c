a0cc5bcb83b259f1b0da93c3184ad690f5dea383
mtk23909 <jibin.xu@mediatek.com>
2022-05-05 16:53:09 +0800

    [ALPS07200210] DISP: resolve the bug about nightlight
    [Detail]
    resolve the conflict between nightlight and bluelight.
    do nothing when the night light feature config.
    
    MTK-Commit-Id: 8b0e3e4e0a4f6dba400a989cf62a9df2aa90ba6e
    
    Change-Id: I7564d1e5e6f887325560adf848f1d17115a7ff71
    CR-Id: ALPS07200210
    Feature: display_mobile
    Signed-off-by: mtk23909 <jibin.xu@mediatek.com>

--- drivers/gpu/drm/mediatek/mtk_disp_ccorr.c
+++ drivers/gpu/drm/mediatek/mtk_disp_ccorr.c
@@ -672,6 +672,9 @@ int disp_ccorr_set_color_matrix(struct mtk_ddp_comp *comp,
 		DDPPR_ERR("%s: cmdq can not be NULL\n", __func__);
 		return -EFAULT;
 	}
+	if (identity_matrix && (disp_ccorr_number == 1) &&
+			(!g_prim_ccorr_force_linear) && (disp_ccorr_linear & 0x01))
+		return ret;
 
 	if (g_disp_ccorr_coef[id] == NULL) {
 		DDPINFO("%s: can't set matrix for NULL coef", __func__);
