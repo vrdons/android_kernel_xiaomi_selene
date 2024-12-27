5c1c995eed8a1f6b5530046196f60ba83d950e3a
fukiame <fukiame@proton.me>
2024-12-27 21:25:38 +0700

    drivers/misc: eccci: unconditionally kill traffic monitor
    Signed-off-by: fukiame <fukiame@proton.me>
    Co-Authored-By: Wahid Khan <wahidzk0091@gmail.com>

--- drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
+++ drivers/misc/mediatek/eccci/hif/ccci_hif_dpmaif.c
@@ -4394,7 +4394,9 @@ int dpmaif_stop(unsigned char hif_id)
 	/* rx rx clear */
 	dpmaif_stop_rx_sw(hif_id);
 	/* stop debug mechnism */
+#if DPMAIF_TRAFFIC_MONITOR_INTERVAL
 	del_timer(&dpmaif_ctrl->traffic_monitor);
+#endif
 
 	#ifdef MT6297
 	/* todo: CG set */
