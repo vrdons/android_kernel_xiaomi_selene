30bd4907742f1d118e28c1f5f86c2e78aacb514e
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-05-15 22:55:24 +0300

    mediatek: leds/ktd3136_bl: Disable verbose debug

--- drivers/misc/mediatek/leds/mt6768/ktd3136_bl.c
+++ drivers/misc/mediatek/leds/mt6768/ktd3136_bl.c
@@ -24,12 +24,14 @@
 struct ktd3137_chip *bkl_chip;
 int ktd_hbm_mode;
 
-#define KTD_DEBUG
+#define KTD_DEBUG 0
 
 #ifdef KTD_DEBUG
 #define LOG_DBG(format, args...) do { \
 	pr_debug("[ktd]"format"\n", ##args);\
 } while (0)
+#else
+#define LOG_DBG(format, args...) ((void)0)
 #endif
 
 int ktd3137_brightness_table_reg4[256] = {0x01, 0x02, 0x04, 0x04, 0x07,
