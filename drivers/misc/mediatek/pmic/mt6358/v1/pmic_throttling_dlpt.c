0712e6c955b35765c3b64bf4978bc37c3ca271d4
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-05-21 17:50:52 +0300

    drivers/misc/mediatek/pmic: Very minor code improvement

--- drivers/misc/mediatek/pmic/mt6358/v1/pmic_throttling_dlpt.c
+++ drivers/misc/mediatek/pmic/mt6358/v1/pmic_throttling_dlpt.c
@@ -1237,10 +1237,9 @@ void dlpt_notify_init(void)
 
 	dlpt_notify_thread = kthread_run(dlpt_notify_handler, 0,
 		"dlpt_notify_thread");
-	if (IS_ERR(dlpt_notify_thread))
-		pr_debug("Failed to create dlpt_notify_thread\n");
-	else
-		pr_debug("Create dlpt_notify_thread : done\n");
+	if (!dlpt_notify_thread) {
+		pr_notice("Failed to create dlpt_notify_thread\n");
+	}
 }
 
 #else
