c9e7635b76de55c51d69dc2b48177061bc0430b1
Deniz <vrdons@proton.me>
2025-11-02 03:56:28 +0300

    drivers/mediatek/teei: Refactor TEEI_BOOT_FOOTPRINT macro for logging

--- drivers/misc/mediatek/teei/400/tz_driver/teei_client_main.c
+++ drivers/misc/mediatek/teei/400/tz_driver/teei_client_main.c
@@ -60,10 +60,6 @@
 #include "tz_log.h"
 
 #if defined(CONFIG_MICROTRUST_TZ_DRIVER_MTK_BOOTPROF) && defined(CONFIG_MTPROF)
-
-#if KERNEL_VERSION(4, 19, 0) <= LINUX_VERSION_CODE
-#define TEEI_BOOT_FOOTPRINT(str) bootprof_log_boot(str)
-#else
 #define TEEI_BOOT_FOOTPRINT(str) log_boot(str)
 #else
 #define TEEI_BOOT_FOOTPRINT(str) IMSG_PRINTK("%s\n", str)
